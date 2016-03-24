414dfc20e7878ad17cfd02adbeea60005c2ef25c
pejo1 <per.jonsson@betssongroup.com>
2016-03-24 08:47:34 +0100

    Added two-way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,28 @@
 import {Component} from 'angular2/core'
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
+
 @Component({
     selector: 'ng2-preybird-app',
-    template: '<h1>ng2-preybird</h1>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
+      `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public title = "Preybird's heroes";
+    public hero: Hero = {
+        id: 1,
+        name: 'Jimmy Smith'
+    };
+}
