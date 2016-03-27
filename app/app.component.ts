e962af691eefe1fe28303259c9942ff73da00166
perjo927 <perjo927@student.liu.se>
2016-03-27 10:38:25 +0200

    feat(routing): update routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
     selector: 'ng2-preybird-app',
     template: `
         <h1>{{title}}</h1>
-        <a [routerLink]="['Heroes']">Heroes</a>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
         <router-outlet></router-outlet>
     `,
     directives: [ROUTER_DIRECTIVES],
@@ -22,7 +26,13 @@ import { HeroesComponent } from './heroes.component';
         path: '/heroes',
         name: 'Heroes',
         component: HeroesComponent
-    }
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
 ])
 
 export class AppComponent {
